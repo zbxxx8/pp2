ca8a9af8308f247597cf95de0cbd6ea6a9ae84a2
zhansulu <zhans.bx@gmail.com>
2023-02-03 20:47:52 +0600

    for loops

--- lab2/155.py
+++ lab2/155.py
@@ -1,3 +1,5 @@
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
+  if x == "banana":
+    break
---
+++ lab2/158.py
@@ -0,0 +1,7 @@
+for x in range(6):
+  print(x) 
+
+for x in range(2, 6):
+  print(x)
+
+  
